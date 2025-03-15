b332ce0ca120894beb3d20378cacb0266cea86d7
Lasy <47871724+lramovha@users.noreply.github.com>
2025-03-15 23:03:57 +0200

    simple array
    2D array c++

--- practice_code/twoDimentionalArray.cpp
+++ practice_code/twoDimentionalArray.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+
 int main(){
-    
+    int matrix [2][3] = {{2, 5, 8},{7, 6, 2}};
+    cout << "matrix:\n";
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 3; j++){
+           cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
