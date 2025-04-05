cf2411fcc15eb3b9a2d85b0f04ce3cc0b51436ee
Lasy <47871724+lramovha@users.noreply.github.com>
2025-04-05 20:50:22 +0200

    InsertAtEnd
    created an insertAtEnd function.

--- practice_code/linkedLists.cpp
+++ practice_code/linkedLists.cpp
@@ -1,17 +1,34 @@
 #include <iostream>
 using namespaces std;
 
+//Define the structure of the node
 struct Node{
-    int data;
-    Node *next;
+    int data;   //data variable
+    Node *next;     // pointer variable
 }
 
-Node *head = nullptr;
+Node *head = nullptr;   //Head to track start of list
 
 void insertAtStart(int value){
-    Node *newNode = new Node();
-    newNode -> data = value;
-    newNode -> *next = head;
-    head = newNode;
+    Node *newNode = new Node();     //create new node instance.
+    newNode -> data = value;        //assign data with value input.
+    newNode -> next = head;         //assign next pointer points to the current head pointer
+    head = newNode;                 //head pointer now points to newNode.
     cout << value << "inserted at the biginning.\n";
+}
+
+void insertAtEnd(int value){
+    Node *newNode = new Node();     //create newNode instance.
+    newNode -> data = value;        //assign value to data.
+    newNode -> next = nullptr;      //last node points to null.
+
+    if (head == nullptr){
+        head = newNode;
+    }
+    else{
+        Node *tempPtr = head;
+        while (newNode -> next != nullptr){
+
+        }
+    }
 }
