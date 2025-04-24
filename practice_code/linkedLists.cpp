702a591f03f1311264cf8d7e8c06efe1bda5a00d
Lasy <47871724+lramovha@users.noreply.github.com>
2025-04-25 00:05:32 +0200

    added comment
    update code comments.

--- practice_code/linkedLists.cpp
+++ practice_code/linkedLists.cpp
@@ -7,10 +7,10 @@ struct Node{
     Node *next;     // pointer variable
 };
 
-Node *head = nullptr;   //Head to track start of list
+Node *head = nullptr;   //Head pointer to track start of list
 
 void insertAtBeginning(int value){
-    Node *newNode = new Node();     //create new node instance.
+    Node *newNode = new Node();     //creates just a new node instance.
     newNode -> data = value;        //assign data with value parameter.
     newNode -> next = head;         //assign next pointer points to the current head pointer
     head = newNode;                 //head pointer now points to newNode.
@@ -20,42 +20,42 @@ void insertAtBeginning(int value){
 void insertAtEnd(int value){
     Node *newNode = new Node();     //create newNode instance.
     newNode -> data = value;        //assign value to data.
-    newNode -> next = nullptr;      //last node points to null.
+    newNode -> next = nullptr;      //upon creation pointer points to null.
 
     if (head == nullptr){ // if there's no node.
-        head = newNode;
+        head = newNode; // head is equal to new created node.
     }
     else{
-        Node *tempPtr = head;
-        while (tempPtr -> next != nullptr){ // Transverse to the last node
-            tempPtr = tempPtr -> next;
+        Node *temp = head; //else creates temp pointer, pointing to head.
+        while (temp -> next != nullptr){ // Transverse to the last node before null pointer.
+            temp = temp -> next; //keep increamenting pointer to next for iteration.
         }
-        tempPtr -> next = newNode;  // Link the newNode to the last node(nullptr)
+        temp -> next = newNode;  // assigns newNode to temporary pointer before null.
     }
-    cout << value << "inserted at the end.\n";
+    cout << value << " inserted at the end.\n";
 }
 
 void displayList() {
-    if (head == nullptr) {
+    if (head == nullptr) {  // if the list is empty viz. head equal null.
         cout << "This List is empty.\n";
         return;
     }
-    Node *temp = head;
+    Node *temp = head;  //create temporary pointer of Node type.
     cout << "LinkedList: ";
-    while (temp != nullptr){
-        cout << temp -> data << " -> ";
-        temp = temp ->next;
+    while (temp != nullptr){    //while pointer is not null pointer viz. end of linkedlist
+        cout << temp -> data << " -> "; //print temporary pointer data.
+        temp = temp ->next;     //incriment tempory pointer.
     }
-    cout << "NULL\n";
+    cout << "NULL\n"; //print null to show end of linked list.
 }
 
 void deleteAtBeginning(){
-    if (head == nullptr){
+    if (head == nullptr){   //if the linked list is empty viz. head is null pointer.
         cout << "List is already empty.\n";
         return;
     }
-    Node *temp = head;
-    head = head -> next; //move temporary head to next node in line.
+    Node *temp = head;      //create temporary node that poins to head viz. first node of linkedList.
+    head = head -> next; //move temporary head to next node in line viz. second node.
     cout << "Deleted " << temp -> data << " from the beginning.\n";
     delete temp;
 }
@@ -65,17 +65,17 @@ void deleteAtEnd() {
         cout << "List is already empty.\n";
         return;
     }
-    if (head -> next == nullptr){ // If there's only one node 
+    if (head -> next == nullptr){ // If there's only one node viz. if next node after head is null.
         cout << "Deleted " << head -> data << " from the end.\n";
-        delete head;
-        head = nullptr;
+        delete head; // free's head memory.
+        head = nullptr; //points head back to nullptr viz. linkedList is now empty.
         return;
     }
     Node *temp = head;
     while (temp -> next -> next != nullptr) { //Transverse to second last
         temp = temp -> next;
     }
-    cout << "Deleted " << temp -> next -> data << " from the end";
+    cout << "Deleted " << temp -> next -> data << " from the end.\n";
     delete temp -> next;    // delete last node.
     temp -> next = nullptr;
 }
@@ -85,6 +85,14 @@ int main(){
     insertAtEnd(20);
     insertAtBeginning(5);
     insertAtEnd(30);
+
     displayList(); // Should display: 5 -> 10 -> 20 -> 30 -> NULL
+
+    deleteAtBeginning();
+    displayList(); // Should display: 10 -> 20 -> 30 -> NULL
+
+    deleteAtEnd();
+    displayList(); // Should display: 10 -> 20 -> NULL
+
     return 0;
 }
